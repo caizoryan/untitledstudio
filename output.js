e6dddbb0e05e43a3319143e1669a94ae05c50788
Aaryan Pashine <pashineaaryan@gmail.com>
2025-09-28 18:04:06 -0400

    autplay

--- output.js
+++ output.js
@@ -85,7 +85,7 @@ html += `
 			`
  if (website.type == "video") {
 html += `
-				<video class="hidden" webkit-playsinline playsinline loop src="`
+				<video class="hidden" autoplay webkit-playsinline playsinline loop src="`
 
 html += website.cover
 
